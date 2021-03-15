694eda4243e9f62442b84b34e83d6f52920c3185
Колчин Леонид Евгеньевич <lekolchin@sberbank.ru>
2021-03-15 03:13:42 +0300

    jest setup cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,7 @@
-// // eslint-disable-next-line @typescript-eslint/no-var-requires
-// const { pathsToModuleNameMapper } = require('ts-jest/utils');
-// // eslint-disable-next-line @typescript-eslint/no-var-requires
-// const { compilerOptions } = require('./tsconfig');
-
 module.exports = {
   preset: 'ts-jest',
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest'
-  },
-  globals: {
-    'babel-jest': {
-      tsConfig: 'tsconfig.json'
-    }
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   modulePaths: ['<rootDir>/src'],
