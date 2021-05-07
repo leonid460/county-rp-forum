de150a0f519abf33212ae9af1b2de4033d235a16
Колчин Леонид Евгеньевич <lekolchin@sberbank.ru>
2021-05-07 18:48:31 +0300

    add margin-bottom in PageContentContainer

--- src/ui-kit/atoms/PageContentContainer/styled.ts
+++ src/ui-kit/atoms/PageContentContainer/styled.ts
@@ -5,6 +5,11 @@ export const ContentContainer = styled.div`
   width: 100%;
   border-radius: 4px;
   margin-top: ${Gaps.Gap20};
+  margin-bottom: ${Gaps.Gap24};
   overflow: hidden;
   background: ${({ theme }) => theme.colors.surface};
+
+  ${({ theme }) => theme.breakpoints.up('m')} {
+    margin-bottom: ${Gaps.Gap32};
+  }
 `;
